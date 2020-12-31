77532aed9cf572517d9b8c45b365c4a21fe65014
zhanghexuan <1479406235@qq.com>
2020-12-31 14:58:07 +0800

    通过sigaction设置只响应来自内核的信号

---
+++ signal/slowcat_sigaction.c
@@ -0,0 +1,105 @@
+/*
+ * 通过sigaction设置只响应来自内核的信号
+ * zhanghexie
+ * 2020-12-31
+ */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define CPS 10
+#define BUFSIZE CPS
+#define BURST 100
+
+// volatile 保证不被优化成常量
+// sig_atomic_t 保证读写都是由一条指令完成的
+static volatile sig_atomic_t token = 0;
+static void alarm_handler(int s, siginfo_t* infoPrt, void* unused) {
+  alarm(1);
+  if (infoPrt->si_code != SI_KERNEL) {
+    return;
+  }
+  ++token;
+  token = token > BURST ? BURST : token;
+}
+
+int main(int argc, char* argv[]) {
+  // 出错时返回负值不要用char。
+  int sfd, dfd = 1;
+  char buf[BUFSIZE];
+
+  if (argc < 2) {
+    fprintf(stderr, "Usage:%s <src_file>\n", argv[0]);
+    exit(1);
+  }
+
+  struct sigaction sa;
+  sa.sa_sigaction = alarm_handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_SIGINFO;
+  sigaction(SIGALRM, &sa, NULL);
+  alarm(1);
+
+  do {
+    sfd = open(argv[1], O_RDONLY);
+    if (sfd < 0) {
+      if (errno != EINTR) {
+        perror("open()");
+        exit(1);
+      }
+    }
+  } while (sfd < 0);
+
+  int len, ret;
+  while (1) {
+    // 有令牌才能读
+    while (token <= 0) {
+      pause();
+    }
+    --token;
+
+    // 读
+    while (len = read(sfd, buf, BUFSIZE)) {
+      if (len < 0) {
+        if (errno != EINTR) {
+          perror("read()");
+          exit(0);
+        }
+      } else {
+        break;
+      }
+    }
+    if (0 == len) {
+      break;
+    }
+
+    // 写
+    int pos = 0;
+    while (len > 0) {
+      while (ret = write(dfd, buf + pos, len)) {
+        if (ret < 0) {
+          if (errno != EINTR) {
+            perror("write()");
+            exit(0);
+          }
+        } else {
+          break;
+        }
+      }
+      if (ret < 0) {
+        exit(1);
+      }
+      pos += ret;
+      len -= ret;
+    }
+  }
+  close(sfd);
+  exit(0);
+}
